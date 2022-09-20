4672bb3156b1225d39b62751e682e96d4c3be983
407 <407@academicos.cesde.edu.co>
2022-09-20 17:40:57 -0500

    ok

---
+++ src/helpers/constantesGET.js
@@ -0,0 +1,6 @@
+const TOKEN="Bearer BQApia5y8Klwi_aQQ3c2jcgIGh5JEHQmeW7PwnSfV2gjqe6Lp8TJtuECsZI3xsojKAFXjksNlMCOz1RdgdrdAmD7lUKgYT-147saEPKww3B9Cwa892GJ-urFTNETzWHJPSpi9ALrq1HzXE3B39JetRVWd91LlrwEMN5X2mlZPfq2NpwO2DY"
+
+export const PETICION={
+    method:"GET",
+    headers:{Authorization:TOKEN},
+}
--- src/services/servicioConsultarCanciones.js
+++ src/services/servicioConsultarCanciones.js
@@ -1,24 +1,16 @@
-//RECETA PARA CONSUMIR APIS CON JS PURO
-
-//1. si yo quiero consumir un api
-//debo saber PARA DONDE IR Y A QUE SERVICIO....
-//DEBO CONFIGURAR LA URI
-const URI="https://api.spotify.com/v1/artists/3fMbdgg4jU18AjLCKBhRSm/top-tracks?market=US"
-
-//2. Configuro datos Especiales o DE CONTROL en el servidor
-const TOKEN="Bearer BQApaasNLjhYS_eZuxag8joweizUunJkRHR42cOdTMjoWoxs-zD7vpqesM7NpimRHybluED5ELtWoRTj-pKkH0I9eaSUx7UF4z7OF7EN2bVUOWRGt9ghmu3AfcY774Lr13WC01c2SeS4ZI9msKrgnej7HdejM66ikoSkAELUAEEDIk7JJ96yV-NkXM4Yw9p5I9M"
-
-//3. Configuro la peticion
-//NOTA: SOLO POST Y PUT CONFIGURAN BODY
-//PARA JS LA PETICION ES UN OBJETO
-const PETICION={
-    method:"GET",
-    headers:{Authorization:TOKEN},
-}
-
-//4. ARRANQUE PUES MIJO
-//CONSUMA EL API
-fetch(URI,PETICION)
+let URI="https://api.spotify.com/v1/artists/2ye2Wgw4gimLv2eAKyk1NB/top-tracks?market=US";
+let buscar =document.getElementById("buscar")
+buscar.addEventListener("click",function(evento){
+    evento.preventDefault()
+    let artista=document.getElementById("artista").value 
+    console.log(artista)
+    URI=`https://api.spotify.com/v1/artists/${artista}/top-tracks?market=US`
+    const TOKEN="Bearer BQApia5y8Klwi_aQQ3c2jcgIGh5JEHQmeW7PwnSfV2gjqe6Lp8TJtuECsZI3xsojKAFXjksNlMCOz1RdgdrdAmD7lUKgYT-147saEPKww3B9Cwa892GJ-urFTNETzWHJPSpi9ALrq1HzXE3B39JetRVWd91LlrwEMN5X2mlZPfq2NpwO2DY"
+    const PETICION={
+        method:"GET",
+        headers:{Authorization:TOKEN},
+    }
+    fetch(URI,PETICION)
 .then(function(respuesta){
     return respuesta.json() //garantizo formato JSON
 })
@@ -27,14 +19,44 @@ fetch(URI,PETICION)
     console.log(respuesta.tracks) //accedo a un atributo de la respuesta
 
     //recorrer un arreglo
+    let fila=document.getElementById("fila")
+    fila.innerHTML = ""
     respuesta.tracks.forEach(function(cancion){
-        console.log(cancion)
+        /*console.log(cancion)
         console.log(cancion.name)
         console.log(cancion.preview_url)
         console.log(cancion.album.images[0].url)
         console.log(cancion.popularity)
         console.log(cancion.album.name)
-        console.log(cancion.album.release_date)
+        console.log(cancion.album.release_date)*/
+        let columna=document.createElement("div")
+        columna.classList.add("col")
+
+        let tarjeta=document.createElement("div")
+        tarjeta.classList.add("card","h-100","shadow")
+
+        let audio=document.createElement("audio")
+        audio.classList.add("w-100")
+        audio.setAttribute("controls","controls")
+        audio.src=cancion.preview_url
+
+        let imagen=document.createElement("img")
+        imagen.classList.add("h-100","w-100","img-fluid")
+        imagen.src=cancion.album.images[0].url
+
+        let nombre=document.createElement("p")
+        nombre.textContent=cancion.name  
+
+        //PADRES E HIJOS
+        tarjeta.appendChild(imagen)
+        tarjeta.appendChild(audio)
+        tarjeta.appendChild(nombre)
+        columna.appendChild(tarjeta)
+        fila.appendChild(columna)
+
+
+       
+
 
 
 
@@ -46,3 +68,25 @@ fetch(URI,PETICION)
 .catch(function(respuestaError){
     console.log(respuestaError)
 })
+})
+
+
+
+
+//RECETA PARA CONSUMIR APIS CON JS PURO
+
+//1. si yo quiero consumir un api
+//debo saber PARA DONDE IR Y A QUE SERVICIO....
+//DEBO CONFIGURAR LA URI
+
+
+//2. Configuro datos Especiales o DE CONTROL en el servidor
+
+
+//3. Configuro la peticion
+//NOTA: SOLO POST Y PUT CONFIGURAN BODY
+//PARA JS LA PETICION ES UN OBJETO
+
+
+//4. ARRANQUE PUES MIJO
+//CONSUMA EL API
